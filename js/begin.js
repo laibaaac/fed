48e6d68e7bb37bb5b7a0c70e805e13837fc5c27b
Laiba Choudhry <laiba@hotmail.nl>
2022-12-15 14:51:27 +0100

    yikes

--- js/begin.js
+++ js/begin.js
@@ -1,11 +1,14 @@
 const deMenuButton = document.querySelector("header > button");
 const deNav = document.querySelector("header > nav");
 const deSluitButton = document.querySelector("header nav button");
+const nieuwenav=document.querySelector("")
+
 
 deMenuButton.addEventListener("click", toggleMenu);
 deSluitButton.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-	deNav.classList.toggle("open");
+	deNav.classList.remove('open')
+    nieuwenav.classList.add('open')
 }
 
